22bcaf316121d13974dab080bbc756505e1a55fc
angle6460 <ad.parra.r@gmail.com>
2025-11-03 17:07:44 +1100

    adding models

---
+++ Models/WorkLoads/IWorkLoad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkScedulerApp.Models.WorkLoads;
+
+public interface IWorkLoad
+{
+    string Name { get; set; }
+
+    double CalculateHours();
+    string GetDetails();
+    void Load(string key, Dictionary<string, string> value);
+    Tuple<string, Dictionary<string, string>> Save();
+}
---
+++ Models/WorkLoads/PerItemWorkLoad.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkScedulerApp.Models.WorkLoads;
+
+public class PerItemWorkLoad : IWorkLoad
+{
+    private string _name = "PerItem";
+
+    public PerItemWorkLoad()
+    {
+        
+    }
+    public double CalculateHours()
+    {
+        throw new System.NotImplementedException();
+    }
+    public string Name {
+        get
+        {
+            return _name;
+        }
+        set
+        {
+            _name = value;
+        }
+    }
+
+    public string GetDetails()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Load(string key, Dictionary<string, string> value)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Tuple<string, Dictionary<string, string>> Save()
+    {
+        throw new System.NotImplementedException();
+    }
+}

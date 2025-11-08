1a7a783ca67451cd4ee64ea6ce9d383a3b7f3fba
angle6460 <ad.parra.r@gmail.com>
2025-11-08 11:19:30 +1100

    finishedc

--- Models/Scheduling/ShiftBreak.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WorkSchedulerApp.Models.Scheduling;
-
-public class ShiftBreak
-{
-    public int BreakId { get; set; }
-    public int ShiftId { get; set; }
-    public DateTime BreakTime { get; set; }
-}
